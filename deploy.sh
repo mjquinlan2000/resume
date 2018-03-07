96e454019f30055b7ab5736803d690fa75dffef5
Mike Quinlan <mquinlan@enbala.com>
2018-03-07 16:41:07 -0700

    use-webpack: Fixing typos

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-gulp clean
-NODE_ENV=production gulp build
-gsutil rsync -R ./dist gs://www.mikequinlan.rocks
+gsutil cp ./index.html gs://www.mikequinlan.rocks
+gsutil cp ./images/favicon.ico gs://www.mikequinlan.rocks/images
+gsutil cp ./styles/application.css gs://www.mikequinlan.rocks/styles
 gsutil acl ch -u AllUsers:R \
   'gs://www.mikequinlan.rocks/*.html' \
   'gs://www.mikequinlan.rocks/images/*' \
